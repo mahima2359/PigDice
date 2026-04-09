7a234d70c3a6c699b1ca87440bc33b7a9dfdb9aa
mahima2359 <mahimachouksey04@gmail.com>
2026-04-09 22:32:18 +0530

    fixed dice image issue final

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ rollDice.addEventListener('click', function () {
     //Display dice image
     diceImg.classList.remove('hidden');
     //Add random dice img to img src
-    diceImg.src = `images/dice-${dice}.png`;
+    diceImg.src = `./images/dice-${dice}.png`;
     //Check whether it's 1 and change player
     if (dice !== 1) {
       //Keep the current score to the current active player
@@ -92,6 +92,7 @@ holdDice.addEventListener('click', function () {
         .querySelector(`.player--${activePlayer}`)
         .classList.remove('player--active');
       diceImg.classList.add('hidden');
+      diceImg.src = './images/dice-1.png';
     } else {
       //switch player
       playerSwitching();
